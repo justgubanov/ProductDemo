3ee4f742cc6aaec161c1d29f343dccd7e00636ac
Alex <gubanov1514@gmail.com>
2020-12-16 01:10:29 +0300

    ClosingViewController added

---
+++ ProductDemo/View/ClosingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ClosingViewController.swift
+//  ProductDemo
+//
+//  Created by Александр Губанов on 16.12.2020.
+//
+
+import UIKit
+
+class ClosingViewController: UIViewController {
+
+    @IBAction func dismissSelf(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
